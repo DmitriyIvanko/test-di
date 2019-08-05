46c4256a01c3877304af19c3e0258155253fdb70
DmitriyIvanko <ivanko.dmitriy@gmail.com>
2019-08-05 14:57:34 +0300

    + temp save;

--- src/app/heroes/hero-list/hero-list.module.ts
+++ src/app/heroes/hero-list/hero-list.module.ts
@@ -3,21 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { HeroListRoutingModule } from './hero-list-routing.module';
 import { HeroListComponent } from './hero-list.component';
-// import { HeroListService } from './hero-list.service';
 
 @NgModule({
   declarations: [
     HeroListComponent,
   ],
-  // exports: [
-  //   HeroListComponent,
-  // ],
   imports: [
     CommonModule,
     HeroListRoutingModule,
   ],
-  // providers: [
-  //   HeroListService,
-  // ],
 })
 export class HeroListModule { }
